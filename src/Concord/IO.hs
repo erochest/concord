fa18f787550d4df0899a8b1f47224310d6040840
Eric Rochester <erochest@virginia.edu>
2015-02-05 21:45:46 -0500

    Added conduits to yield lines from a directory of files.

--- src/Concord/IO.hs
+++ src/Concord/IO.hs
@@ -4,13 +4,39 @@
 
 module Concord.IO
     ( walkCorpus
+    , toLines
+    -- , toCorpusLines
     ) where
 
 
 import           Conduit
+import           Control.Applicative
+import           Control.Monad
+import qualified Data.Conduit.Text         as CT
+import qualified Data.Text                 as T
+import qualified Data.Text.Lazy            as TL
 import           Filesystem.Path.CurrentOS
 import           Prelude                   hiding (FilePath)
+
+import           Concord.Types
 
 
 walkCorpus :: MonadResource m => FilePath -> Producer m FilePath
 walkCorpus = sourceDirectoryDeep True
+
+toLines :: MonadResource m => Conduit FilePath m (Located T.Text)
+toLines = do
+    fp' <- await
+    case fp' of
+        Just fp -> sourceFile fp =$= CT.lines =$= nLines 0 fp
+        Nothing -> return ()
+
+nLines :: MonadResource m
+       => Int -> FilePath -> ConduitM T.Text (Located T.Text) m ()
+nLines n fp =   await
+            >>= maybe (return ())
+                      ( (>> nLines (n + 1) fp)
+                      . yield
+                      . Located fp (Line n))
+
+-- toCorpusLines :: MonadResource m => Conduit (Located T.Text) m (Located
--- src/Concord/Types.hs
+++ src/Concord/Types.hs
@@ -1,4 +1,38 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+
 module Concord.Types
-    (
+    ( Located(..)
+    , locatedIn
+    , locatedAt
+    , locatedData
+
+    , Location(..)
+    , _Line
+    , lineNo
+    , _LineChar
+    , lineChar
     ) where
 
+
+import           Control.Lens
+import           Filesystem.Path.CurrentOS
+import           Prelude                   hiding (FilePath)
+
+
+data Location
+        = Line     { _lineNo :: !Int
+                   }
+        | LineChar { _lineNo   :: !Int
+                   , _lineChar :: !Int
+                   }
+        deriving (Show)
+makePrisms ''Location
+makeLenses ''Location
+
+data Located a = Located
+               { _locatedIn   :: !FilePath
+               , _locatedAt   :: !Location
+               , _locatedData :: !a
+               } deriving (Show)
+makeLenses ''Located
